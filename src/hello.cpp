139cf7dd6bda590b867b2af0b364ad653332a692
thirdparty5214 <useforthirdpartys@gmail.com>
2024-11-13 12:28:35 +0530

    add src folder and src files

---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include the header file for input/output stream
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print "Hello, World!" to the console
+    return 0;  // Return 0 to indicate successful execution
+}
